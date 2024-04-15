c0d2bc3122cd1325372e4b1b6bb520f9badcff3f
bim-oulabi <m.oulabi@bimventures.com>
2024-04-15 16:17:23 +0300

    Update use-nav.tsx

--- src/components/layouts/dashboard-layout/use-nav.tsx
+++ src/components/layouts/dashboard-layout/use-nav.tsx
@@ -16,7 +16,7 @@ import {
 import { useTranslations } from 'use-intl';
 
 type NavItem = {
-  title: Translation;
+  title: TranslationKey;
   shortcut: string;
   href: string;
 };
@@ -47,25 +47,30 @@ export const UserNav = () => {
   return (
     <DropdownMenu dir={dir}>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
-          <Avatar className='h-8 w-8'>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
             <AvatarFallback>MO</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end' className='w-52' sideOffset={8}>
+      <DropdownMenuContent align="end" className="w-52" sideOffset={8}>
         <DropdownMenuLabel>
-          <div className='flex flex-col'>
-            <div className='mb-1 text-sm leading-none'>mohammadou</div>
-            <div className='text-xs text-muted-foreground'>it.moh.ou@gmail.com</div>
+          <div className="flex flex-col">
+            <div className="mb-1 text-sm leading-none">mohammadou</div>
+            <div className="text-xs text-muted-foreground">
+              it.moh.ou@gmail.com
+            </div>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           {nav_items.map((item) => (
             <DropdownMenuItem key={item.title + item.href} asChild>
-              <NextLink href={`dashboard${item.href}`} className='flex justify-between'>
-                <span className='grow'>{t(item.title)}</span>
+              <NextLink
+                href={`dashboard${item.href}`}
+                className="flex justify-between"
+              >
+                <span className="grow">{t(item.title)}</span>
                 <DropdownMenuShortcut>{item.shortcut}</DropdownMenuShortcut>
               </NextLink>
             </DropdownMenuItem>
